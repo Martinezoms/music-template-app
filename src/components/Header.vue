c00ea894e86437a193426e46c66e534fb0a6e9e0
Martins Enyinnaya <martinezoms15@gmail.com>
2024-07-27 21:51:40 +0100

    Update: setup router for the project and built manage page UI for the managing musics

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,23 +3,32 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+      >
+        Music
+      </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }"> About </router-link>
+          </li>
           <li v-if="!userLoggedIn">
-            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
-              >Login / Register</a
-            >
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">
+              Login / Register
+            </a>
           </li>
           <template v-else>
             <li v-if="userLoggedIn">
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
+              <a class="px-2 text-white" href="#" @click.prevent="logout">Logout</a>
             </li>
           </template>
         </ul>
@@ -44,8 +53,15 @@ export default {
 
   methods: {
     ...mapActions(useUserStore, ['signOut']),
+
     toggleAuthModal() {
       this.isAuthModalOpen = !this.isAuthModalOpen
+    },
+
+    logout() {
+      this.signOut()
+
+      if (this.$route.meta.requiresAuth) this.$router.push({ name: 'home' })
     }
   }
 }
