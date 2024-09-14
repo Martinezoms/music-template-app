b343cf8f229750b631e09f55170199478b36faf8
Martins Enyinnaya <martinezoms15@gmail.com>
2024-09-14 14:33:46 +0100

    Update: translating HTML using component interpolation

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -3,10 +3,24 @@ import en from '@/locales/en.json'
 import fr from '@/locales/fr.json'
 
 export default createI18n({
-  locale: 'en',
+  locale: 'fr',
   fallbackLocale: 'en',
   messages: {
     en,
     fr
+  },
+  numberFormats: {
+    en: {
+      currency: {
+        style: 'currency',
+        currency: 'USD'
+      }
+    },
+    fr: {
+      currency: {
+        style: 'currency',
+        currency: 'EUR'
+      }
+    }
   }
 })
